8b156ba9fd9a1ec14b87a98c0039e9bfbf79c43d
Avery Stahl <averystahl@hotmail.com>
2019-12-07 10:35:23 -0500

    start description / js set up

---
+++ script.js
@@ -0,0 +1,17 @@
+// start at beginning with a start button
+// get the start button
+var body = document.body;
+var start = document.querySelector(".start");
+var questionText = document.querySelector(".question");
+var choices = document.querySelectorAll(".choices");
+// when the start button is clicked it will remove the start button 
+// when the start button is clicked it will replace the directions with answer choices
+// when the start button is clicked it will replace the h1 tag with questions
+// the questions will be pulled from an object with an array of items
+
+function startQuiz()
+{
+    
+}
+
+start.addEventListener("click", startQuiz());
